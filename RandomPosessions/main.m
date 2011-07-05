33b5f5a7e86b95d888f5374c4c8fedcd2bdf0f36
Jerrad Thramer <Jerrad.Thramer@gmail.com>
2011-07-05 10:57:53 -0500

    Initial Commit

---
+++ RandomPosessions/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RandomPosessions
+//
+//  Created by Jerrad Thramer on 7/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
